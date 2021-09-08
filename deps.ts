f7cebac17187ba0df792082d5168a0f6e55587a5
Schufeli <me@schufi.codes>
2021-09-08 14:20:02 +0200

    Added: deps.ts for easier caching when building docker image

---
+++ deps.ts
@@ -0,0 +1,3 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import axiod from 'https://deno.land/x/axiod/mod.ts';
+import { parse } from 'https://deno.land/std@0.82.0/encoding/yaml.ts';
